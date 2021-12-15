39b68efd0b1faf14a8337131dbf9f359ddc571e0
Ditte Lykke Hansen <dittehansen143@gmail.com>
2021-12-15 23:59:28 +0100

    Slideshow

---
+++ craftddk/craftddk.js
@@ -0,0 +1,70 @@
+//Loops are handy, if you want to run the same code over and over again, each time with a different value 
+// https://www.w3schools.com/howto/howto_js_slideshow.asp 
+// https://www.w3schools.com/js/js_loop_for.asp
+
+
+//Laver variabler til billederne og til dots'ene
+var image0 = document.getElementById('image0');
+var image1 = document.getElementById('image1');
+var image2 = document.getElementById('image2');
+var image3 = document.getElementById('image3');
+var image4 = document.getElementById('image4');
+
+var dot0 = document.getElementById('dot0');
+var dot1 = document.getElementById('dot1');
+var dot2 = document.getElementById('dot2');
+var dot3 = document.getElementById('dot3');
+var dot4 = document.getElementById('dot4');
+
+//Sætter det aktive Id til at være 0, så det har en værdi
+var activeId = 0;
+
+//Her starter funktionen, når man (onclick) på img med pilene
+//Hvis det aktive Id IKKE er lig med 4, så skal den pluse sig selv med én
+//Hvis det aktive Id ER lig med 4, så skal den skifte tilbage til 0
+function nextImage(){
+    if(activeId !== 4){
+        activeId = activeId + 1;
+    } else {
+        activeId = 0;
+    }
+
+    // Her laver den en loop, for at hurtig læse en masse forskellige værdier
+    // Sætter en ny variabel til at være 0 hvergang funktionen bliver kørt. 
+    // Så længe variablen "i" er mindre end 5, så vil den pluse sig med 1 (sådan så loopet forsætter, med at køre)
+    // Når i pluser sig selv så den er lige 5, så stopper den loopet
+    // Når den kører loopet, så tjekker den hvad i er og hvad det aktive Id er, sådan så den tilføjer og fjerne classes
+    for (let i = 0; i < 5; i++) {
+        if(i == activeId){
+            document.getElementById('image' + i).classList.add('imageActive');
+            document.getElementById('dot' + i).classList.add('dotActive');
+        } else {
+            document.getElementById('image' + i).classList.remove('imageActive');
+            document.getElementById('dot' + i).classList.remove('dotActive');
+        }
+    }
+}
+
+// Ny funktion, samme princip som ovenover
+// Det er (onclick) på den anden pil
+// Hvis det aktive Id IKKE er lig med 0, så skal den minuse sig selv med én
+// Hvis det aktive Id ER lig med 0, så skal den skifte tilbage til 4
+function previousImage(){
+    if(activeId !== 0){
+        activeId = activeId - 1;
+    } else {
+        activeId = 4;
+    }
+
+    // Her er det samme loop, bare i den anden funktion
+    // Loopet gør præcis det samme som i funktionen ovenover
+    for (let i = 0; i < 5; i++) {
+        if(i == activeId){
+            document.getElementById('image' + i).classList.add('imageActive');
+            document.getElementById('dot' + i).classList.add('dotActive');
+        } else {
+            document.getElementById('image' + i).classList.remove('imageActive');
+            document.getElementById('dot' + i).classList.remove('dotActive');
+        }
+    }
+}
